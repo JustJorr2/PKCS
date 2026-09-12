86f972f536420941568485cd7ffaa6256d10e372
Jordan <icey2728@gmail.com>
2026-09-12 22:22:48 +0800

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 // API Configuration and base URL
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://sarapanpagipge.com/";
 
 export const config = {
   API_BASE_URL,
